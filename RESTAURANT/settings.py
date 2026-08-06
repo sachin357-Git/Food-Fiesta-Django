5de81b000835bd1eeba220b684b1692891ba616c
sachin357-Git <tube357.corp@gmail.com>
2026-08-05 19:59:00 -0700

    fix static images

--- RESTAURANT/settings.py
+++ RESTAURANT/settings.py
@@ -121,9 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_DIR=BASE_DIR/"static"
-STATICFILES_DIRS=[STATIC_DIR]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
